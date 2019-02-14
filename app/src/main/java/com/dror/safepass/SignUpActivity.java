51bd4de2bed0850acb6f0168d7b5325f1e4d7e0d
idor17 <idor17@gmail.com>
2019-02-14 11:20:56 +0200

    open screen

--- app/src/main/java/com/dror/safepass/SignUpActivity.java
+++ app/src/main/java/com/dror/safepass/SignUpActivity.java
@@ -2,12 +2,28 @@ package com.dror.safepass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextUsername, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonSignUp:
+
+                break;
+
+            case R.id.
+
+        }
     }
 }
