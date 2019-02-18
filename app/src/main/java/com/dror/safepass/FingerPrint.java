c0729db08f00e46834f0ab6de3913c5580ca93ff
DrorArie <Dror803@gmail.com>
2019-02-18 18:22:17 +0200

    new

--- app/src/main/java/com/dror/safepass/FingerPrint.java
+++ app/src/main/java/com/dror/safepass/FingerPrint.java
@@ -1,27 +1,95 @@
 package com.dror.safepass;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FingerPrint extends AppCompatActivity  implements View.OnClickListener {
+
+    FirebaseAuth mAuth;
+    EditText editTextEmail , editTextPassword;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finger_print);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+
         findViewById(R.id.SignUp).setOnClickListener(this);
+        findViewById(R.id.Login).setOnClickListener(this);
     }
+
+    private void userLogin() {
+
+
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(FingerPrint.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case  R.id.SignUp:
-
                 startActivity(new Intent(this, SignUpActivity.class ));
+                break;
 
+            case R.id.Login:
+                userLogin();
                 break;
 
 
--- app/src/main/java/com/dror/safepass/SignUpActivity.java
+++ app/src/main/java/com/dror/safepass/SignUpActivity.java
@@ -1,28 +1,113 @@
 package com.dror.safepass;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText editTextUsername, editTextPassword;
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword, editTextConfirmedPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextConfirmedPassword = findViewById(R.id.confirmPassword);
+        progressBar = findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.loginScreen).setOnClickListener(this);
+
     }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String confimedPassword = editTextConfirmedPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(confimedPassword.isEmpty()){
+            editTextConfirmedPassword.setError("Please confirm your password");
+            editTextConfirmedPassword.requestFocus();
+            return;
+        }
+        if (!(confimedPassword.equals(password))){
+            editTextConfirmedPassword.setError("Password aren't equal");
+            editTextConfirmedPassword.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else{
+                    if(task.getException() instanceof FirebaseAuthInvalidUserException){
+                        Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.buttonSignUp:
-
+                registerUser();
                 break;
 
-            case R.id.
+            case R.id.loginScreen:
+                startActivity(new Intent(this, FingerPrint.class));
+                break;
 
         }
     }
