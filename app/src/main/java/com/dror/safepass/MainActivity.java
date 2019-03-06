7d59ac4e95183a4626fce02b49f7add3697f30cc
DrorArie <Dror803@gmail.com>
2019-03-06 21:49:37 +0200

    new

--- app/src/main/java/com/dror/safepass/MainActivity.java
+++ app/src/main/java/com/dror/safepass/MainActivity.java
@@ -1,21 +1,74 @@
 package com.dror.safepass;
 
-import android.graphics.drawable.AnimationDrawable;
-import android.support.constraint.ConstraintLayout;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ConstraintLayout constraintLayout = findViewById(R.id.layout);
-        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
-        animationDrawable.setEnterFadeDuration(2000);
-        animationDrawable.setExitFadeDuration(4000);
-        animationDrawable.start();
+        mAuth =FirebaseAuth.getInstance();
+
+        editText = findViewById(R.id.editText);
+        progressBar = findViewById(R.id.progressBar);
+
+
+        findViewById(R.id.button_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserInformation();
+            }
+        });
+    }
+
+    private void saveUserInformation() {
+        String displayName = editText.getText().toString();
+        progressBar.setVisibility(View.VISIBLE);
+
+        if(displayName.isEmpty()){
+            editText.setError("Name required");
+            editText.requestFocus();
+            return;
+        }
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user != null){
+            UserProfileChangeRequest profile = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(displayName).build();
+
+            user.updateProfile(profile)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if(task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Name Updated", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+
+
     }
 }
